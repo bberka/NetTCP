4e7f7f262ef07e0808fa0f985ec455c105ca289a
bberka <berkaybyr@icloud.com>
2024-04-17 00:39:23 +0300

    feat(HandlerErrorEventArgs.cs)

---
+++ src/NetTCP.Client/Events/HandlerErrorEventArgs.cs
@@ -0,0 +1,12 @@
+﻿namespace NetTCP.Client.Events;
+
+public sealed class HandlerErrorEventArgs
+{
+  public NetTcpClient Client { get; }
+  public Exception Exception { get; }
+
+  internal HandlerErrorEventArgs(NetTcpClient client, Exception exception) {
+    Client = client;
+    Exception = exception;
+  }
+}
---
+++ src/NetTCP.Server/Events/HandlerErrorEventArgs.cs
@@ -0,0 +1,12 @@
+﻿namespace NetTCP.Server.Events;
+
+public sealed class HandlerErrorEventArgs
+{
+  public NetTcpConnection Connection { get; }
+  public Exception Exception { get; }
+
+  internal HandlerErrorEventArgs(NetTcpConnection connection, Exception exception) {
+    Connection = connection;
+    Exception = exception;
+  }
+}
